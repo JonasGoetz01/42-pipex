1a2377ff633f71affed5bcf10cdafc399137a5ae
Jonas Götz <jonas.goetz01@web.de>
2023-11-15 17:09:50 +0100

    add util functions to header file

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jgotz <jgotz@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 14:14:38 by jgotz             #+#    #+#             */
-/*   Updated: 2023/11/14 10:49:53 by jgotz            ###   ########.fr       */
+/*   Updated: 2023/11/15 17:08:36 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 
 # include "../lib/printf/ft_printf.h"
 # include <fcntl.h>
+# include <stdio.h>
 # include <unistd.h>
+
+void	write_error(char *str);
+void	free_pipes(int index, int **pipes);
+void	create_pipes(int pnum, int ***pipes);
+void	close_pipes(int i, int pnum, int ***pipes);
+void	close_pipes_parent(int pnum, int ***pipes);
 
 #endif
